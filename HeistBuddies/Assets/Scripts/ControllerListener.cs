8b613f3caf2aad8ba723b46c9ec1e8b37f75ed2c
PedroNeves <133124266+PedroNevesDev@users.noreply.github.com>
2024-11-07 18:16:13 +0000

    Input bugfixes

--- HeistBuddies/Assets/Scripts/ControllerListener.cs
+++ HeistBuddies/Assets/Scripts/ControllerListener.cs
@@ -23,48 +23,67 @@ public class ControllerListener : MonoBehaviour
 
     void Update()
     {
-        if(!p1 || !p2) // If players doesnt exist spanw them
+        // If players doesnt exist spanw them
+        if(!p1 || !p2) 
             SpawnPlayers();
 
-        if(numberOfGamepads!=Gamepad.all.Count) // Checking for Controllers Count
+        // Checking for Controllers Count
+        if(numberOfGamepads!=Gamepad.all.Count) 
             OnDeviceChange();
     }
     void OnDeviceChange()
     {
-        numberOfGamepads=Gamepad.all.Count; // Update numberOfPads so that it doesnt call OnDeviceChange multiple times
+        // Update numberOfPads so that it doesnt call OnDeviceChange multiple times
+        numberOfGamepads=Gamepad.all.Count; 
 
-        multiplayer=numberOfGamepads>=2; // Assigning multiplayer bool for better readability 
+        // Assigning multiplayer bool for better readability 
+        multiplayer=numberOfGamepads>=2; 
 
+        Debugging();
 
-        //Debugging
-        print("Gamepad count: " + numberOfGamepads);
-        print("Multiplayer Mode: " + multiplayer);
-
-        if(multiplayer) // Changing Controller Schemes depending on the boolean. true == multiplayer, false == singleplayer  
+        // Changing Controller Schemes depending on the boolean. true == multiplayer, false == singleplayer  
+        if(multiplayer) 
         {
             p1.SwitchCurrentControlScheme("Multiplayer", new InputDevice[] {Gamepad.all[0]});
             p2.SwitchCurrentControlScheme("Multiplayer", new InputDevice[] {Gamepad.all[1]});
         }
         else        
         {
-            p1.SwitchCurrentControlScheme("Player1", new InputDevice[] { Keyboard.current, Mouse.current, Gamepad.all[0]});
-            p2.SwitchCurrentControlScheme("Player2", new InputDevice[] { Keyboard.current, Mouse.current, Gamepad.all[0]});
-        }
+     
 
-    
+            p1.SwitchCurrentControlScheme("Player1", GetDefaultDevices());
+            p2.SwitchCurrentControlScheme("Player2", GetDefaultDevices());
+        }
+    }
+
+    // Just a quick and easy way to get access to the default singleplayer devices without repiting the code
+    InputDevice[] GetDefaultDevices()
+    {
+            if(Gamepad.all.Count==1)
+                return new InputDevice[] { Keyboard.current, Mouse.current, Gamepad.all[0]};
+
+        return new InputDevice[] { Keyboard.current, Mouse.current};
     }
     void SpawnPlayers()
     {
-        p1 = PlayerInput.Instantiate(playerPrefab); // Instantiating player one
+        // Instantiating player one
+        p1 = PlayerInput.Instantiate(playerPrefab); 
 
-        p1.SwitchCurrentControlScheme("Player1", new InputDevice[] { Keyboard.current, Mouse.current, Gamepad.all[0]});
+        p1.SwitchCurrentControlScheme("Player1", GetDefaultDevices());
         p1.transform.position = mapSpawnPoint.position + Vector3.left*1;
         cameraTargetGroup.AddMember(p1.GetComponent<PlayerController>().Rb.transform,1,1);
+        
+        // Instantiating player two
+        p2 = PlayerInput.Instantiate(playerPrefab); 
 
-        p2 = PlayerInput.Instantiate(playerPrefab); // Instantiating player two
-
-        p2.SwitchCurrentControlScheme("Player2", new InputDevice[] { Keyboard.current, Mouse.current, Gamepad.all[0]});
+        p2.SwitchCurrentControlScheme("Player2", GetDefaultDevices());
         p2.transform.position = mapSpawnPoint.position + Vector3.left*2;
         cameraTargetGroup.AddMember(p2.GetComponent<PlayerController>().Rb.transform,1,1);
+    }
+
+    void Debugging()
+    {
+        print("Gamepad count: " + numberOfGamepads);
+        print("Multiplayer Mode: " + multiplayer);
     }
 }
