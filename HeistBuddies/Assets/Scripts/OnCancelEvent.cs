9728d4f17df900822ba76588b16c5c60e4d2585e
PedroNevesDev <133124266+PedroNevesDev@users.noreply.github.com>
2025-01-08 20:56:22 +0000

    SpashScreen stuff

--- HeistBuddies/Assets/Scripts/OnCancelEvent.cs
+++ HeistBuddies/Assets/Scripts/OnCancelEvent.cs
@@ -6,14 +6,13 @@ using UnityEngine.InputSystem.UI;
 
 public class OnCancelEvent : MonoBehaviour
 {
-    private InputSystemUIInputModule inputModule;
+    [SerializeField] InputSystemUIInputModule inputModule;
     [SerializeField]UnityEvent onCancel = new UnityEvent();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     void Start()
     {
-        var eventSystem = FindObjectOfType<UnityEngine.EventSystems.EventSystem>();
-        inputModule = eventSystem?.GetComponent<InputSystemUIInputModule>();
+        inputModule = FindFirstObjectByType<InputSystemUIInputModule>();
     }
 
     private void OnDisable() 
